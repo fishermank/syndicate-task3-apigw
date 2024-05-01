dbbc76d2242eb36418959b57b66ea3562ea99282
Artem_Kulomin <artem_kulomin@epam.com>
2024-05-01 18:11:13 +0300

    Task 4

---
+++ task04/src/lambdas/sqs_handler/handler.py
@@ -0,0 +1,23 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('SqsHandler-handler')
+
+
+class SqsHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        return event
+
+
+HANDLER = SqsHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
---
+++ task04/tests/test_sqs_handler/test_success.py
@@ -0,0 +1,7 @@
+from tests.test_sqs_handler import SqsHandlerLambdaTestCase
+
+
+class TestSuccess(SqsHandlerLambdaTestCase):
+
+    def test_success(self):
+        pass
