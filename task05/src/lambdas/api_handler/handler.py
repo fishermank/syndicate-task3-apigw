2d7fd03f04d7272beaaea8e953a4416843a645df
Artem_Kulomin <artem_kulomin@epam.com>
2024-05-03 15:32:31 +0300

    Task5

---
+++ task05/src/lambdas/api_handler/handler.py
@@ -0,0 +1,23 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('ApiHandler-handler')
+
+
+class ApiHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        return 200
+    
+
+HANDLER = ApiHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
