cebb3f67bea1cfce89f23d64edf76b3aaf0652d0
Artem_Kulomin <artem_kulomin@epam.com>
2024-05-10 19:35:37 +0300

    Task 8

---
+++ task08/src/lambdas/api_handler/handler.py
@@ -0,0 +1,55 @@
+import json
+from pprint import pprint
+
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('ApiHandler-handler')
+
+
+import asyncio
+
+from open_meteo import OpenMeteo
+from open_meteo.models import DailyParameters, HourlyParameters
+
+
+class MeteoForecast:
+
+    @staticmethod
+    async def _main():
+        """Show example on using the Open-Meteo API client."""
+        async with OpenMeteo() as open_meteo:
+            forecast = await open_meteo.forecast(
+                latitude=52.27,
+                longitude=6.87417,
+                current_weather=True,
+                hourly=[
+                    HourlyParameters.TEMPERATURE_2M,
+                    HourlyParameters.RELATIVE_HUMIDITY_2M,
+                ],
+            )
+            result = json.loads(forecast.json())
+
+        pprint(result)
+        return result
+
+    def get_weather_forecast(self):
+        return asyncio.run(self._main())
+
+
+class ApiHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        mf = MeteoForecast()
+        forecast = mf.get_weather_forecast()
+        return forecast
+
+
+HANDLER = ApiHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
